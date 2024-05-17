d9af21503324cc3392e399c8eb7e60c31f615706
Dylan Bennett <dylan@hatchcoding.com>
2024-05-17 18:45:08 -0400

    Added in neighbour counting logic

--- src/App.js
+++ src/App.js
@@ -54,29 +54,73 @@ class App extends Component {
         cells: createCells(),
     }
 
+    // Increment the cell's state, wrapping around
     incrementCellState = (cell) => {
         cell.state = (cell.state + 1) % numCellStates;
         return cell;
     };
 
+    getNumNeighbours = (cell, cells) => {
+        const neighbourIndices = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+
+        return neighbourIndices.reduce((numNeighbours, neighbour) => {
+            const neighbourRow = cell.row + neighbour[0];
+            const neighbourCol = cell.col + neighbour[1];
+
+            const isNeighbourOnBoard = (neighbourRow >= 0 && neighbourRow < rows && neighbourCol >= 0 && neighbourCol < cols);
+            const isNeighbourAlive = isNeighbourOnBoard && cells[neighbourRow][neighbourCol].state > 0;
+
+            return numNeighbours + isNeighbourAlive;
+        }, 0);
+    };
+
+    // Handler function for clicking on a cell
     handleCellClick = (cell) => {
-        const updatedCells = (prevState) => {
+        const updateCells = (prevState) => {
             // Make a deep copy of the cells
             const newCells = JSON.parse(JSON.stringify(prevState.cells));
 
-            // Update the desired cell
+            // Increment the cell's state
             newCells[cell.row][cell.col] = this.incrementCellState(cell);
             return newCells;
         }
 
         this.setState((prevState) => ({
-            cells: updatedCells(prevState)
+            cells: updateCells(prevState)
+        }));
+    };
+
+    // Handler function for moving the entire board forward one step
+    handleStepClick = () => {
+        const updateAllCells = (prevState) => {
+            // Make a deep copy of the cells
+            const newCells = JSON.parse(JSON.stringify(prevState.cells));
+
+            // Go through each cell and update its state based on the Game of Life rules
+            for (let r = 0; r < rows; r++) {
+                for (let c = 0; c < cols; c++) {
+                    // Get the number of neighbours of that cell
+                    newCells[r][c].neighbours = this.getNumNeighbours(newCells[r][c], prevState.cells);
+
+                    // Update the cell's state based on the rules
+                    // TODO
+                }
+            }
+
+            return newCells;
+        };
+
+        this.setState((prevState) => ({
+            cells: updateAllCells(prevState)
         }));
     };
 
     render() {
         return (
-            <><Grid cells={this.state.cells} onCellClick={this.handleCellClick} /></>
+            <>
+                <Grid cells={this.state.cells} onCellClick={this.handleCellClick} />
+                <div><button type="button" onClick={this.handleStepClick}>Step</button></div>
+            </>
         );
     }
 }
